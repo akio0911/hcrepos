3b8c4de0f2733121c533d3648019d7181d77332e
yuiseki <yuiseki@gmail.com>
2009-01-06 02:09:08 +0900

    iroiro

--- Ruby4Skype/skype_job.rb
+++ Ruby4Skype/skype_job.rb
@@ -5,19 +5,34 @@ require 'cgi'
 require 'kconv'
 require 'time'
 require 'skypeapi'
+require 'uri'
+require 'open-uri'
+require 'rexml/document'
+
+require 'mitter'
+require 'hc_blogger'
+
 SkypeAPI.init
 SkypeAPI.attachWait
 test = "#voqn_skype/$6410ca0139e195d0"
 yuiseki = "#yuiseki/$97c57c5363208f6a"
 hack = "#akio0911/$yuiseki;1600dfa22ed008f5"
 
-
+videos = Mitter.logs_of_groups
 videos.each do |video|
   how_ago = (Time.now - video[:time])
-  #puts how_ago.to_i.to_s + " " + video[:time].to_s
   if how_ago.to_i <= 60*6
     string = video[:user] + " によって、Hacker'sCafeグループに動画が投稿されました:\n" + video[:title] + " " + video[:url]
     SkypeAPI::ChatMessage.create(hack, string)
+  end
+end
+
+videos = Mitter.logs_of_users
+videos.each do |video|
+  how_ago = (Time.now - video[:time])
+  if how_ago.to_i <= 60*6
+    string = video[:user] + "がまた動画Mitter: " + video[:title] + " " + video[:url]
+    SkypeAPI::ChatMessage.create(test, string)
   end
 end
 
