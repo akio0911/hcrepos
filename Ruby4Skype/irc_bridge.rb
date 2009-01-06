629626c391d40381204b3f5444cecc77ada23f8c
takano32 <takano32@gmail.com>
2009-01-06 14:56:01 +0900

    * irc_bridge.rb: refactor * skype_job.rb: driver comment

--- Ruby4Skype/irc_bridge.rb
+++ Ruby4Skype/irc_bridge.rb
@@ -6,42 +6,61 @@ require 'kconv'
 
 $KCODE = 'UTF8'
 
-@server = "irc.freenode.net"
-@port = 6666
-@eol = "\r\n"
-@nick = "skype_bot"
-@channel = "#hackerscafe"
+class IrcClient
+	def initialize
+		@irc = TCPSocket.new(@server, @port)
+		@server = "irc.freenode.net"
+		@port = 6666
+		@eol = "\r\n"
+		@nick = "skype_bot"
+		@channel = "#hackerscafe"
+	end
+	
+	def send_cmd(cmd)
+		p "Sending command..... :#{cmd}"
+		@irc.write(cmd + @eol)
+	end
 
-def send_cmd(cmd)
-	p "Sending command..... :#{cmd}"
-	@irc.write(cmd + @eol)
-end
+	def login_and_join
+		send_cmd("USER skype_bot, #{@server}, ignore, Hacker's Cafe")
+		send_cmd("NICK #{@nick}")
+		send_cmd("JOIN #{@channel}")
+	end
+	
+	def read_thread
+		read_thread = Thread.new do
+			Thread.stop
+			while msg = Kconv.toutf8(@irc.gets).split
+				p msg.join(' ')
+				send_cmd("PONG #{msg[1]}") if msg[0] == 'PING'
+			end
+		end
+	end
 
-def login_and_join
-	send_cmd("USER skype_bot, #{@server}, ignore, Hacker's Cafe")
-	send_cmd("NICK #{@nick}")
-	send_cmd("JOIN #{@channel}")
-end
+	def write_thread
+		write_thread = Thread.new do
+			Thread.stop
+			while input = gets.chop
+				send_cmd("PRIVMSG #{@channel} #{Kconv.tojis(input)}")
+			end
+		end
+	end
 
-read = Thread.new do
-	Thread.stop
-	while msg = Kconv.toutf8(@irc.gets).split
-		p msg.join(' ')
-		send_cmd("PONG #{msg[1]}") if msg[0] == 'PING'
+	def start
+		read_thread.run
+		login_and_join
+		write_thread.run
+	end
+
+	def stop
+		read.join
+		write.join
 	end
 end
 
-write = Thread.new do
-	Thread.stop
-	while input = gets.chop
-		send_cmd("PRIVMSG #{@channel} #{Kconv.tojis(input)}")
-	end
+
+if __FILE__ == $0 then
+	@client = IrcClient.new
+	@client.start
 end
 
-@irc = TCPSocket.new(@server, @port)
-
-read.run
-login_and_join
-write.run
-read.join
-write.join
