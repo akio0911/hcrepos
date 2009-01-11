6951323bad03cb453a949bb19cbeab8f0104e60e
takano32 <takano32@gmail.com>
2009-01-12 03:03:57 +0900

    confirm to up quizknow.new on Rails 2.2.2

--- iknow-api/quizknow.new/config/routes.rb
+++ iknow-api/quizknow.new/config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :conversions
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -17,12 +19,6 @@ ActionController::Routing::Routes.draw do |map|
 
   # Sample resource route with sub-resources:
   #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-  
-  # Sample resource route with more complex sub-resources
-  #   map.resources :products do |products|
-  #     products.resources :comments
-  #     products.resources :sales, :collection => { :recent => :get }
-  #   end
 
   # Sample resource route within a namespace:
   #   map.namespace :admin do |admin|
@@ -31,13 +27,13 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "conversions"
+
+  map.connect 'new_request/:username', :controller => "oauth"
 
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
-  # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing the them or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
