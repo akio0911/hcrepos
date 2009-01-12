97c46399d29a40aa3741592d04519117c349c024
takano32 <takano32@gmail.com>
2009-01-12 17:18:03 +0900

    make directory for developing SkypeIrcGateway

---
+++ Ruby4Skype/sig/sig.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+# 
+# Copyright (c) 2009 TAKANO Mitsuhiro <tak at no32.tk>
+#
+# sig.rb: SkypeIrcGateway
+# 建設予定地
+#
+
+require 'irc_bridge'
+requrie 'skype_bridge'
+
+
