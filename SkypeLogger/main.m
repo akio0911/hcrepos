7ba4c6c167221ce7708e08e3889c4c6647e8cd24
akio0911 <akio0911@gmail.com>
2009-01-12 16:17:04 +0900

    add SkypeLogger project

---
+++ SkypeLogger/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SkypeAPITest
+//
+//  Created by Janno Teelem on 14/04/2005.
+//  Copyright 2005-2006 Skype Limited. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
