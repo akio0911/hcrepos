9103c1275dcd9fc563fafb54ddc4ec930915423e
takano32 <takano32@gmail.com>
2009-01-14 05:48:37 +0900

    sig.rb: fix conditions when received messages

--- SkypeIrcGateway/sig.rb
+++ SkypeIrcGateway/sig.rb
@@ -23,14 +23,14 @@ class SkypeIrcGateway
 	
 	def skype_initialize
 		@skype_client.receive_message do |channel, name, message|
-			@irc_client.send_message("#{name}: #{message}") if channel == @skype_chat
+			@irc_client.send_message("#{name}: #{message}") unless channel == name
 		end
 		@skype_client.start
 	end
 
 	def irc_initialize
 		@irc_client.receive_message do |channel, name, message|
-			@skype_client.send_message("(swear) #{name}: #{message}") unless channel == name
+			@skype_client.send_message("(swear) #{name}: #{message}") if channel == @skype_client
 		end
 		@irc_client.start
 	end
