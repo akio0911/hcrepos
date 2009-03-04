e389f8655c5505180a9cdbb2750d9a98c9b4a723
akio0911 <akio0911@gmail.com>
2009-03-04 14:22:39 +0900

    add dev_open

--- lang/ruby/tds01v/tds01v.rb
+++ lang/ruby/tds01v/tds01v.rb
@@ -7,6 +7,13 @@ include Termios
 DEVICE = '/dev/tty.usbserial-00001004'
 BAUDRATE = B9600
 
+def dev_open(path)
+  dev = open(DEVICE, File::RDWR | File::NONBLOCK)
+  mode = dev.fcntl(Fcntl::F_GETFL, 0)
+  dev.fcntl(Fcntl::F_SETFL, mode & ~File::NONBLOCK)
+  dev
+end
+
 dev = dev_open(DEVICE)
 
 # % cu -s 9600 -l /dev/tty.usbserial-0000103D
@@ -55,3 +62,24 @@ dev = dev_open(DEVICE)
 # RES 0042 007D FDED 0C2C 0004 FFFB FFB1 FFE5 FFEA 27B8 FFE3 0137 0B17 # センサ情報 全センサ全データ(ベクトルデータ+計測データ)
 
 # ~.
+
+# sudo gem install termios
+# Building native extensions.  This could take a while...
+# Successfully installed termios-0.9.4
+# 1 gem installed
+
+# port search termios
+# rb-termios @0.9.4 (ruby, sysutils)
+#     simple wrapper for termios(3)
+
+# sudo port install rb-termios
+# --->  Fetching rb-termios
+# --->  Attempting to fetch ruby-termios-0.9.4.tar.gz from http://distfiles.macports.org/ruby
+# --->  Verifying checksum(s) for rb-termios
+# --->  Extracting rb-termios
+# --->  Configuring rb-termios
+# --->  Building rb-termios
+# --->  Staging rb-termios into destroot
+# --->  Installing rb-termios @0.9.4_0
+# --->  Activating rb-termios @0.9.4_0
+# --->  Cleaning rb-termios
