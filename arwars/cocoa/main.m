d19b8e8e1e379141cfb9e0a0efc7f3cacaf1e11b
akio0911 <akio0911@gmail.com>
2009-05-22 00:40:39 +0900

    add arwars/cocoa project

---
+++ arwars/cocoa/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  cocoa
+//
+//  Created by akio0911 on 09/04/05.
+//  Copyright akio0911 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
