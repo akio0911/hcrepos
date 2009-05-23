7174793e9ce3a67fdccbcdb22d74ef3273e9a265
akio0911 <akio0911@gmail.com>
2009-05-23 18:25:48 +0900

    scaleを追加

--- event/mtm03/SimpleCube/Data/pat.rb
+++ event/mtm03/SimpleCube/Data/pat.rb
@@ -10,10 +10,14 @@ pat = [
   end
 end
 
+scale = 4
+x_max = (pat[0].size * scale) - 1
+y_max = (pat.size * scale)- 1
+
 0.upto(2) do |i|
-  0.upto(15) do |y|
-    0.upto(15) do |x|
-      print sprintf "%4d", pat[y/4][x/4]
+  0.upto(y_max) do |y|
+    0.upto(x_max) do |x|
+      print sprintf "%4d", pat[y/scale][x/scale]
     end
     print "\n"
   end
@@ -21,9 +25,9 @@ end
 print "\n"
 
 0.upto(2) do |i|
-  15.downto(0) do |x|
-    0.upto(15) do |y|
-      print sprintf "%4d", pat[y/4][x/4]
+  x_max.downto(0) do |x|
+    0.upto(y_max) do |y|
+      print sprintf "%4d", pat[y/scale][x/scale]
     end
     print "\n"
   end
@@ -31,9 +35,9 @@ end
 print "\n"
 
 0.upto(2) do |i|
-  15.downto(0) do |y|
-    15.downto(0) do |x|
-      print sprintf "%4d", pat[y/4][x/4]
+  (y_max).downto(0) do |y|
+    x_max.downto(0) do |x|
+      print sprintf "%4d", pat[y/scale][x/scale]
     end
     print "\n"
   end
@@ -41,9 +45,9 @@ end
 print "\n"
 
 0.upto(2) do |i|
-  0.upto(15) do |x|
-    15.downto(0) do |y|
-      print sprintf "%4d", pat[y/4][x/4]
+  0.upto(x_max) do |x|
+    y_max.downto(0) do |y|
+      print sprintf "%4d", pat[y/scale][x/scale]
     end
     print "\n"
   end
