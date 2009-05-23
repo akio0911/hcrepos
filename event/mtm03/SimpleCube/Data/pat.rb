d7175a2f7bb15c13c9c2c2226ccd1b64b006941e
akio0911 <akio0911@gmail.com>
2009-05-23 18:08:30 +0900

    RGBループを追加

--- event/mtm03/SimpleCube/Data/pat.rb
+++ event/mtm03/SimpleCube/Data/pat.rb
@@ -6,57 +6,46 @@ pat = [
 ]
 0.upto(pat.size-1) do |y|
   0.upto(pat[0].size-1) do |x|
-    pat[y][x] = pat[y][x] * 255
+      pat[y][x] = pat[y][x] * 255
   end
 end
 
-# pat = [
-# [0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0],
-# [0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0],
-# [0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0],
-# [0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0],
-
-# [0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1],
-# [0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1],
-# [0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1],
-# [0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1],
-
-# [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-# [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-# [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-# [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-
-# [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-# [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-# [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-# [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-# ]
-
-0.upto(15) do |y|
-  0.upto(15) do |x|
-    print sprintf "%4d", pat[y/4][x/4]
-  end
-  print "\n"
-end
-print "\n"
-15.downto(0) do |x|
+0.upto(2) do |i|
   0.upto(15) do |y|
-    print sprintf "%4d", pat[y/4][x/4]
+    0.upto(15) do |x|
+      print sprintf "%4d", pat[y/4][x/4]
+    end
+    print "\n"
   end
-  print "\n"
 end
 print "\n"
-15.downto(0) do |y|
+
+0.upto(2) do |i|
   15.downto(0) do |x|
-    print sprintf "%4d", pat[y/4][x/4]
+    0.upto(15) do |y|
+      print sprintf "%4d", pat[y/4][x/4]
+    end
+    print "\n"
   end
-  print "\n"
 end
 print "\n"
-0.upto(15) do |x|
+
+0.upto(2) do |i|
   15.downto(0) do |y|
-    print sprintf "%4d", pat[y/4][x/4]
+    15.downto(0) do |x|
+      print sprintf "%4d", pat[y/4][x/4]
+    end
+    print "\n"
   end
-  print "\n"
+end
+print "\n"
+
+0.upto(2) do |i|
+  0.upto(15) do |x|
+    15.downto(0) do |y|
+      print sprintf "%4d", pat[y/4][x/4]
+    end
+    print "\n"
+  end
 end
 print "\n"
