b5f95d772e22ad751e84f8bcfdf921d178584b65
TAKANO Mitsuhiro <takano32@gmail.com>
2009-05-24 10:03:10 +0900

    add portd stub and inspect method

--- event/mtm03/mirror_ball.rb
+++ event/mtm03/mirror_ball.rb
@@ -9,6 +9,15 @@ require "serialport"
 
 class State < ActiveResource::Base
 	self.site = 'http://cyberia.yuiseki.net/'
+	def portd
+		portd = 0
+		portd |= 0b00000100 if power
+		portd |= 0b00001000 if blue
+		portd |= 0b00010000 if green
+		portd |= 0b00100000 if yellow
+		portd |= 0b01000000 if red
+		return portd
+	end
 end
 
 #5秒おきにRailsに問い合わせて出力する
@@ -23,6 +32,7 @@ loop {
 	puts data.green
 	puts data.yellow
 	puts data.red
+	puts sprintf('%08b', data.portd)
 	#data2 = Shop.find(:first) # 一番最初に投入された古いデータ
 	sleep 5
 }
