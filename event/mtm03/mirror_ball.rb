5e16d65a9092c3d588f28ccdaba525bcc9f7757f
TAKANO Mitsuhiro <takano32@gmail.com>
2009-05-24 09:27:48 +0900

    use active_recod

--- event/mtm03/mirror_ball.rb
+++ event/mtm03/mirror_ball.rb
@@ -2,12 +2,31 @@
 # -*- coding: utf-8 -*-
 
 
+require "rubygems"
+# gem "activeresource"
+require "active_resource"
+require "serialport"
+
+class State < ActiveResource::Base
+	self.site = 'http://cyberia.yuiseki.net/'
+end
+
+#5秒おきにRailsに問い合わせて出力する
+loop {
+	data = State.find(:last) # 最新データ
+	puts data.inspect
+	# puts data.object_id
+	# puts data.class
+	puts "\n"
+	puts data.power # 勝手にメソッドができてる
+	puts data.blue
+	puts data.green
+	puts data.yellow
+	puts data.red
+	#data2 = Shop.find(:first) # 一番最初に投入された古いデータ
+	sleep 5
+}
 
 
-require 'rubygems'
-require 'serialport'
-
-
-'http://cyberia.yuiseki.net/'
 
 
