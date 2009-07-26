eb84dd47eeecd5f9d9b610b9107728d84e670b35
akio0911 <akio0911@gmail.com>
2009-07-26 22:43:58 +0900

    add some lines

--- sandbox/akio0911/prog2.c
+++ sandbox/akio0911/prog2.c
@@ -1,4 +1,5 @@
 #include <GLUT/glut.h>
+#include <stdlib.h>
 
 GLdouble vertex[][3] = {
   {0.0, 0.0, 0.0,},
@@ -26,11 +27,23 @@ int edge[][2] = {
   {3, 7},
 };
 
+void idle(void)
+{
+  glutPostRedisplay();
+}
+
 void display(void)
 {
   int i;
+  static int r = 0;
 
   glClear(GL_COLOR_BUFFER_BIT);
+
+  glLoadIdentity();
+
+  gluLookAt(3.0, 4.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+  glRotated((double)r, 0.0, 1.0, 0.0);
 
   glColor3d(0.0, 0.0, 0.0);
   glBegin(GL_LINES);
@@ -41,15 +54,52 @@ void display(void)
   glEnd();
 
   glFlush();
+
+  if(++r >=360) r = 0;
 }
 
 void resize(int w, int h)
 {
   glViewport(0, 0, w, h);
 
+  glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
   gluPerspective(30.0, (double)w / (double)h, 1.0, 100.0);
-  glTranslated(0.0, 0.0, -5.0);
+
+  glMatrixMode(GL_MODELVIEW);
+}
+
+void mouse(int button, int state, int x, int y)
+{
+  switch(button){
+  case GLUT_LEFT_BUTTON:
+    if(state == GLUT_DOWN){
+      glutIdleFunc(idle);
+    }
+    else{
+      glutIdleFunc(0);
+    }
+    break;
+  case GLUT_RIGHT_BUTTON:
+    if(state == GLUT_DOWN){
+      glutPostRedisplay();
+    }
+    break;
+  default:
+    break;
+  }
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+  switch(key){
+  case 'q':
+  case 'Q':
+  case '\033':
+    exit(0);
+  default:
+    break;
+  }
 }
 
 void init(void)
@@ -64,6 +114,8 @@ int main(int argc, char *argv[])
   glutCreateWindow(argv[0]);
   glutDisplayFunc(display);
   glutReshapeFunc(resize);
+  glutMouseFunc(mouse);
+  glutKeyboardFunc(keyboard);
   init();
   glutMainLoop();
   return 0;
